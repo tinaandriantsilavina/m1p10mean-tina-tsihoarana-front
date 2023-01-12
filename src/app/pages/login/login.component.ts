dc0fd6e276576f2dcda196337c7afaf0fa374db6
Tina Andria <tinaandriaa@gmail.com>
2023-01-12 09:18:45 +0300

    init

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,83 @@
+import { AuthService } from './../../services/auth/auth.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit , AfterViewInit{
+
+  submitted=false;
+  formulaire : any;
+  message="";
+  testdata : any;
+
+  ngAfterViewInit(): void {
+    if(this.authservice.users!=null){
+        this.router.navigate(['/actuel/dashboard']);
+    }
+  }
+
+  constructor(
+    public route:ActivatedRoute,
+    public router:Router,
+    public authservice:AuthService,
+    public formBuilder:FormBuilder) {
+      this.formulaire = new FormGroup({})
+  }
+  ngOnInit() {
+    this.initForm();
+  }
+  onSubmit(): void {
+    this.submitted = true; // ijerena ftsn we ef nanindry an ilay boutton ve izy
+  }
+
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.formulaire.controls;
+  }
+  initForm(){
+    this.formulaire = this.formBuilder.group(
+      {
+        st1:["tsila", [Validators.required]],
+        st2:["123456", [Validators.required]]
+      }
+    ); //methode retourn objet de type FormGroup
+  }
+  testtoken(){
+    this.testdata=localStorage.getItem('users');
+  }
+  connexion(){
+    return new Promise((resolve, reject) => {
+      this.authservice.connexion(this.formulaire.getRawValue()).subscribe(
+        d => {
+          let data = (d as {[key: string]: any})
+          if(data['status'] ==200){
+            console.log(data['data']);
+            localStorage.setItem('users',JSON.stringify(data['data'][0]))
+            localStorage.setItem('token',data['data'][1])
+            this.router.navigate([''])
+              .then(() => {
+                window.location.reload();
+              });
+          }
+          else{
+            this.message="Mot de Passe ou User Incorrecte";
+          }
+        },error => {
+          this.message = <any>error;
+          if(this.message != null){
+          }
+        }
+      );
+    })
+  }
+
+  connexionn(){
+    console.log(this.formulaire.getRawValue());
+  }
+
+}

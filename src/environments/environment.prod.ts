fd64dcc0573d15a742d4923714726fab09a971f7
tinaandriantsilavina <tinaandriantsilavina@gmail.com>
2023-01-15 17:03:24 +0100

    init

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,5 @@ export const environment = {
   production: true
 };
 
-// export const base_url = 'http://192.168.123.101/VMWSWAR/';
-export const base_url = 'http://localhost:8090/VMWSWAR/';
-// export const base_url = 'http://localhost:8090/';
+export const base_url = 'http://localhost:8090/';
+

cd19cd48e5614be66a1b41284f48fd0d8cbfb6c3
tinaandriantsilavina <tinaandriantsilavina@gmail.com>
2023-01-15 22:33:59 +0100

    depot voiture

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { LoginComponent } from './pages/login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InscriptionComponent } from './pages/inscription/inscription.component';
+import { DepotVoitureComponent } from './pages/depot-voiture/depot-voiture.component';
 
 const routes: Routes = [
   { path: 'connexion/loginform', component:  LoginComponent},
   { path :'not-found' , component:NotfoundComponent},
   { path :'inscription' , component:InscriptionComponent},
+  { path :'depot-voiture' , component:DepotVoitureComponent},
   {path: '**', redirectTo:'/not-found'}
 
 ];

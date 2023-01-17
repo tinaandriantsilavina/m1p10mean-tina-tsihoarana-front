8111354bda6169b63e256c0f793663de86af5990
Tina Andria <tinaandriaa@gmail.com>
2023-01-17 17:27:04 +0300

    image upload

--- src/app/components/image-upload/image-upload.component.ts
+++ src/app/components/image-upload/image-upload.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-image-upload',
   templateUrl: './image-upload.component.html',
   styleUrls: ['./image-upload.component.scss']
 })
 export class ImageUploadComponent {
+  url: any;
+  msg = "";
+  image : any;
+  selectFile(event: any) {
+    this.url = ""
+    if (!event.target.files[0] || event.target.files[0].length == 0) {
+      this.msg = 'You must select an image';
+      return;
+    }
+    var mimeType = event.target.files[0].type;
+    if (mimeType.match(/image\/*/) == null) {
+      this.msg = "Only images are supported";
+      return;
+    }
+    this.image = event.target.files[0]
+    var reader = new FileReader();
+    reader.readAsDataURL(this.image);
+    reader.onload = (_event) => {
+      this.msg = "";
+      this.url = reader.result;
+    }
+  }
 
+  deleteFile(){
+    this.url = "";
+    this.image  = null;
+  }
 }
